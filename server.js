2419b680e301349b031b78a0803ab26a99d80619
Gimb0 <bradgimbo@gmail.com>
2018-12-22 20:46:45 +1100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var mongoose = require('mongoose')
+
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.createConnection('mongodb://localhost:27017/messages', {useNewUrlParser: true})
+
+var messages = [
+	{name: 'Gumbi', message: 'Hey'},
+	{name: 'Gimbo', message: 'Hello'}
+]
+
+app.get('/messages', (req, res) => {
+	res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+	messages.push(req.body)
+	io.emit('message', req.body)
+	res.sendStatus(200)
+})
+
+io.on('connection', (socket) => {
+	console.log('A user has connected')
+})
+
+var server = http.listen(3000, () => {
+	console.log('Server is listening on port', server.address().port)
+})
