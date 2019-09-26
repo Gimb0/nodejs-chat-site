cc530e4f1c6eb9f7bdb8773aa6cef15645c64d86
Gimb0 <bradgimbo@gmail.com>
2019-09-26 21:28:07 +1000

    upload old code

--- server.js
+++ server.js
@@ -3,13 +3,10 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
-mongoose.createConnection('mongodb://localhost:27017/messages', {useNewUrlParser: true})
 
 var messages = [
 	{name: 'Gumbi', message: 'Hey'},
@@ -32,4 +29,4 @@ io.on('connection', (socket) => {
 
 var server = http.listen(3000, () => {
 	console.log('Server is listening on port', server.address().port)
-})
+})	
