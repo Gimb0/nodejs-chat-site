d76b31a340568f1fe597926ba3e7318bcba8fc42
Bradley Semlitzky <Bradley.Semlitzky@fdmgroup.com>
2020-11-26 17:11:11 +1100

    start configuring routes and database

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// App
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
@@ -8,19 +9,49 @@ app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
-var messages = [
-	{name: 'Gumbi', message: 'Hey'},
-	{name: 'Gimbo', message: 'Hello'}
-]
+// Data
+var Promise = require('bluebird')
+var Dao = require('./data_layer/dao')
+var RoomRepository = require('./data_layer/room_repository')
+var MessageRepository = require('./data_layer/message_repository')
 
-app.get('/messages', (req, res) => {
-	res.send(messages)
+var defaultRoom = { name: 'Default' }
+
+var appDao = new Dao()
+var roomRepo = new RoomRepository(appDao)
+var messageRepo = new MessageRepository(appDao)
+
+// Initialize database
+roomRepo.createTable()
+roomRepo.addNewRoom("default")
+messageRepo.createTable()
+
+
+
+app.get('/messages/:room', (req, res) => {
+	console.log("received request for messages in a room")
+	console.log(req.params.room)
+	messageRepo.getMessagesByRoom(req.params.room)
+	.then(data => {
+		console.log(data)
+		res.send(data)
+	})
+	.catch(error => {
+		console.log(error)
+	})
+	
 })
 
-app.post('/messages', (req, res) => {
-	messages.push(req.body)
-	io.emit('message', req.body)
-	res.sendStatus(200)
+app.post('/messages/:room', (req, res) => {
+	var room = req.params.room
+	if(!roomRepo.doesRoomExist(room)) {
+		res.sendStatus(400)
+	}
+
+	var user = req.body.user
+	var message = req.body.message
+
+	messageRepo.addNewMessage(user, message, new Date(), room)
 })
 
 io.on('connection', (socket) => {
